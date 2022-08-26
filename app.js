018d059dc83797a276b33fc5e557aa306082e853
hsuyihnin <hsuyihnin27@gmail.com>
2022-08-26 16:30:53 +0630

    savetolocal doesn't work

--- app.js
+++ app.js
@@ -33,29 +33,29 @@ function AddToReceipesListOne ( receipeone )
   const receipeDiv = document.createElement( "div" );
   receipeDiv.classList = `bg-white receipe col-2 px-0`;
   receipeDiv.innerHTML = `
-   <img src=${ receipeone.image } class="w-100 receipe-image" alt=""/>
-  <div class="receipe-body p-2">
-  <span class="id" hidden>${ receipeone.id }</span>
- <h3 class="receipe-title fs-5 fw-bold">${ receipeone.title }</h3>
- <p class="receipe-description">${ receipeone.description }</p>
- <div class="star-widget ">
- <input type="radio" name="rate" id="rate-5">
- <label for ="rate-5" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-4">
- <label for ="rate-4" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-3">
- <label for ="rate-3" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-2">
- <label for ="rate-2" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-1">
- <label for ="rate-1" class="fas fa-star"></label>
-           </div>
- <span class="fs-6">By <a class="fw-bold text-black receipe-author" href="#">${ receipeone.author }</a></span>
+    <img src=${ receipeone.image } class="w-100 receipe-image" alt=""/>
+   <div class="receipe-body p-2">
+   <span hidden>${ receipeone.id }</span>
+  <h3 class="receipe-title fs-5 fw-bold">${ receipeone.title }</h3>
+  <p class="receipe-description">${ receipeone.description }</p>
+  <div class="star-widget ">
+  <input type="radio" name="rate" id="rate-5">
+  <label for ="rate-5" class="fas fa-star"></label>
+  <input type="radio" name="rate" id="rate-4">
+  <label for ="rate-4" class="fas fa-star"></label>
+  <input type="radio" name="rate" id="rate-3">
+  <label for ="rate-3" class="fas fa-star"></label>
+  <input type="radio" name="rate" id="rate-2">
+  <label for ="rate-2" class="fas fa-star"></label>
+  <input type="radio" name="rate" id="rate-1">
+  <label for ="rate-1" class="fas fa-star"></label>
+            </div>
+  <span class="fs-6">By <a class="fw-bold text-black receipe-author" href="#">${ receipeone.author }</a></span>
   
-               <a class="add-button px-2 py-2 mt-1" aria-label="save to bookmark" href="#">
-          <i class="fa-solid fa-heart"></i>
-                     </a>
-                     </div>`;
+                <a class="add-button px-2 py-2 mt-1" aria-label="save to bookmark" href="#">
+           <i class="fa-solid fa-heart"></i>
+                      </a>
+                      </div>`;
 
   container.appendChild( receipeDiv );
   sectionContainer.appendChild( container );
@@ -68,46 +68,11 @@ function AddToReceipesListTwo ( receipetwo )
   const receipeDiv = document.createElement( "div" );
   receipeDiv.classList = `col-2 receipe bg-white px-0`;
   receipeDiv.innerHTML = `
-<img src=${ receipetwo.image } class="w-100 receipe-image" alt=""/>
- <div class="receipe-body p-2">
- <span class="id"  hidden>${ receipetwo.id }</span>
- <h3 class="receipe-title fs-5 fw-bold">${ receipetwo.title }</h3>
- <p class="receipe-description">${ receipetwo.description }</p>
-
-   <div class="star-widget ">
- <input type="radio" name="rate" id="rate-5">
- <label for ="rate-5" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-4">
- <label for ="rate-4" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-3">
- <label for ="rate-3" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-2">
- <label for ="rate-2" class="fas fa-star"></label>
- <input type="radio" name="rate" id="rate-1">
- <label for ="rate-1" class="fas fa-star"></label>
-           </div>
-
- <span class="fs-6">By <a class="fw-bold text-black receipe-author" href="#">${ receipetwo.author }</a></span>
- <a class="add-button py-2 px-2 mt-1"  aria-label="save to bookmark" href="#">
-             <i class="fa-solid fa-heart"></i>
-                     </a>
-</div>`;
-
-  receipeContainer.appendChild( receipeDiv );
-  containerSection.appendChild( receipeContainer );
-}
-
-function AddToReceipesListThree ( receipethree )
-{
-  const section = document.querySelector( ".receipes-container-three" );
-  const container = document.querySelector( ".receipes-three" );
-  const receipeDiv = document.createElement( "div" );
-  receipeDiv.classList = `col-2 receipe bg-white px-0`;
-  receipeDiv.innerHTML = `<img src=${ receipethree.image } class="w-100 receipe-image" alt=""/>
-<div class="receipe-body p-2">
-<span class="id" hidden>${ receipethree.id }</span>
-  <h3 class="receipe-title fw-bold fs-5">${ receipethree.title }</h3>
-  <p class="receipe-description">${ receipethree.description }</p>
+ <img src=${ receipetwo.image } class="w-100 receipe-image" alt=""/>
+  <div class="receipe-body p-2">
+  <span hidden>${ receipetwo.id }</span>
+  <h3 class="receipe-title fs-5 fw-bold">${ receipetwo.title }</h3>
+  <p class="receipe-description">${ receipetwo.description }</p>
 
     <div class="star-widget ">
   <input type="radio" name="rate" id="rate-5">
@@ -122,11 +87,46 @@ function AddToReceipesListThree ( receipethree )
   <label for ="rate-1" class="fas fa-star"></label>
             </div>
 
- <span class="fs-6">By <a class="fw-bold text-black receipe-author" href="#">${ receipethree.author }</a></span>
+  <span class="fs-6">By <a class="fw-bold text-black receipe-author" href="#">${ receipetwo.author }</a></span>
+  <a class="add-button py-2 px-2 mt-1"  aria-label="save to bookmark" href="#">
+              <i class="fa-solid fa-heart"></i>
+                      </a>
+ </div>`;
+
+  receipeContainer.appendChild( receipeDiv );
+  containerSection.appendChild( receipeContainer );
+}
+
+function AddToReceipesListThree ( receipethree )
+{
+  const section = document.querySelector( ".receipes-container-three" );
+  const container = document.querySelector( ".receipes-three" );
+  const receipeDiv = document.createElement( "div" );
+  receipeDiv.classList = `col-2 receipe bg-white px-0`;
+  receipeDiv.innerHTML = `<img src=${ receipethree.image } class="w-100 receipe-image" alt=""/>
+ <div class="receipe-body p-2">
+ <span hidden>${ receipethree.id }</span>
+   <h3 class="receipe-title fw-bold fs-5">${ receipethree.title }</h3>
+   <p class="receipe-description">${ receipethree.description }</p>
+
+     <div class="star-widget ">
+   <input type="radio" name="rate" id="rate-5">
+   <label for ="rate-5" class="fas fa-star"></label>
+   <input type="radio" name="rate" id="rate-4">
+   <label for ="rate-4" class="fas fa-star"></label>
+   <input type="radio" name="rate" id="rate-3">
+   <label for ="rate-3" class="fas fa-star"></label>
+   <input type="radio" name="rate" id="rate-2">
+   <label for ="rate-2" class="fas fa-star"></label>
+   <input type="radio" name="rate" id="rate-1">
+   <label for ="rate-1" class="fas fa-star"></label>
+             </div>
+
+  <span class="fs-6">By <a class="fw-bold text-black receipe-author" href="#">${ receipethree.author }</a></span>
   
-             <a class="add-button px-2 py-2 " aria-label="save to bookmark" href="#">
-            <i class="fa-solid fa-heart"></i>
-                      </a>     </div>`;
+              <a class="add-button px-2 py-2 " aria-label="save to bookmark" href="#">
+             <i class="fa-solid fa-heart"></i>
+                       </a>     </div>`;
 
   container.appendChild( receipeDiv );
   section.appendChild( container );
@@ -169,7 +169,7 @@ function showreceipesToUI ()
       var saveToCartButtons = document.querySelectorAll( ".add-button" );
       saveToCartButtons.forEach( ( button ) =>
       {
-        button.addEventListener( "click", () => console.log('clicked') );
+        button.addEventListener( "click", saveToCart );
       } )
     } ).catch( ( error ) => console.log( error ) );
 
@@ -193,26 +193,6 @@ function showreceipesToUI ()
 }
 
 
- 
-function showreceipesToUI () 
-   { 
-       let receipesArr = []; 
-       fetch( "receipes.json" ) 
-       .then( ( res ) => res.json() ) 
-       .then( ( receipes ) => 
-       { 
-           receipesArr = receipes; 
-           let collectionOne = receipesArr.splice( 0, 5 ); 
-           collectionOne.forEach( ( receipe ) => 
-             AddToReceipesListOne( receipe ) ) 
-         
-           let collectionTwo = receipesArr.splice( 0, 10 ); 
-         collectionTwo.forEach( ( receipe ) => AddToReceipesListTwo( receipe ) ) 
-         
-           let collectionThree = receipesArr.splice( 0, 10 ); 
-           collectionThree.forEach( ( receipe ) => AddToReceipesListThree( receipe ) ) 
-       } ).catch( (error )=> console.log( error)); 
- }
 function saveToCart ( event )
 {
   const receipeRow = event.target.closest( ".receipe" );
@@ -220,53 +200,28 @@ function saveToCart ( event )
   const title = receipeRow.querySelector( ".receipe-title" ).textContent;
   const description = receipeRow.querySelector( ".receipe-description" ).textContent;
   const author = receipeRow.querySelector( ".receipe-author" ).textContent;
-  const id = receipeRow.querySelector( ".id" );
+  const id = receipeRow.querySelector( "span" ).textContent;
+
   let receipe = {
     image: image,
     title: title,
     description: description,
     author: author,
-    id: Number(id),
+    id: Number( id ),
   }
-  console.log('save cart')
-  saveToLocalStorage( receipe );
-}
-// let localValue = JSON.parse( localStorage.getItem( 'favorites' ) ) || [];
-// if ( localValue[ receipe.id ] )
-// {
-//   console.log( 'already saved' );
-//   localValue[ receipe.id ].count++;
 
-// }
-// else
-// {
-//   console.log( '1st time saving' );
-//   localValue[ receipe.id ] = {
-//     image: receipe.image,
-//     author: receipe.author,
-//     title: receipe.title,
-//     description: receipe.description,
-//     count: 1,
-//   }
-// }
-// localStorage.setItem( "favorites", JSON.stringify( localValue ) );
-// }
-
-function saveToLocalStorage ( tappedReceipe ) 
-{
+  console.log( receipe );
   let allReceipes = JSON.parse( localStorage.getItem( "favorites" ) ) || [];
-  console.log( allReceipes )
-  console.log( tappedReceipe)
-  for ( let i = 0; i < allReceipes.length; i++ )
-  {
-    if ( allReceipes[ i ].id !== tappedReceipe.id )
-    {
-      allReceipes.push( tappedReceipe );
-    }
-    else
-    {
-      console.log('same receipe')
-    }
-    localStorage.setItem( "favorites", JSON.stringify( allReceipes ) );
-  }
+
+ for ( let i = 0; i < allReceipes.length; i++ )
+ {
+   if ( allReceipes[ i ].id !== receipe.id )
+   {
+     allReceipes.push(receipe);
+   }
+   
+ }
+  
+  localStorage.setItem( 'favorites', JSON.stringify( allReceipes ) );
 }
+
